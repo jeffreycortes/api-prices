01b635d2c3ce1c4ddaebdc38be7c5a782ce9448d
Jeffrey Cortés <jeffcorcue@colsubsidio.com>
2024-03-12 02:38:45 -0500

    Se termina configuracion de h2 y se crean scripts y PriceEntity

---
+++ src/main/java/com/colsubsidio/pricesapi/domain/PriceEntity.java
@@ -0,0 +1,22 @@
+package com.colsubsidio.pricesapi.domain;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.util.Date;
+//2.147.483.647 signed
+//4.294.967.295 unsigned
+@Entity
+@Table(name="prices")
+public class PriceEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    long id;
+    long brandId; //foreign key de la cadena del grupo (1 = ZARA).
+    Date startDate;
+    Date endDate; //rango de fechas en el que aplica el precio tarifa indicado.
+    long priceList; //Identificador de la tarifa de precios aplicable.
+    long productId; // Identificador código de producto.
+    short priority; //Desambiguador de aplicación de precios. Si dos tarifas coinciden en un rago de fechas se aplica la de mayor prioridad (mayor valor numérico).
+    BigDecimal price; //precio final de venta.
+    String curr; // iso de la moneda.
+}
