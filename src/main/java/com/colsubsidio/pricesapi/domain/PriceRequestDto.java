68dcb61cd0d51ba7060229e412c2ab94a9dd681f
Jeffrey Cortés <jeffcorcue@colsubsidio.com>
2024-03-12 03:25:31 -0500

    Se agrega anotación Builder para PriceRequestDto

--- src/main/java/com/colsubsidio/pricesapi/domain/PriceRequestDto.java
+++ src/main/java/com/colsubsidio/pricesapi/domain/PriceRequestDto.java
@@ -1,8 +1,10 @@
 package com.colsubsidio.pricesapi.domain;
 
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
 public class PriceRequestDto {
     String fechaAplicacion;
     String productoId;
