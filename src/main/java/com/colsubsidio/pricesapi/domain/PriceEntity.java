cb778c7b2f52215cdc346e047dec7079f73be786
Jeffrey Cortés <jeffcorcue@colsubsidio.com>
2024-03-12 03:24:00 -0500

    Se mejoran los tipos de datos d PriceEntity

--- src/main/java/com/colsubsidio/pricesapi/domain/PriceEntity.java
+++ src/main/java/com/colsubsidio/pricesapi/domain/PriceEntity.java
@@ -1,10 +1,13 @@
 package com.colsubsidio.pricesapi.domain;
 
+import lombok.Data;
+
 import javax.persistence.*;
 import java.math.BigDecimal;
 import java.util.Date;
 //2.147.483.647 signed
 //4.294.967.295 unsigned
+@Data
 @Entity
 @Table(name="prices")
 public class PriceEntity {
@@ -14,7 +17,7 @@ public class PriceEntity {
     long brandId; //foreign key de la cadena del grupo (1 = ZARA).
     Date startDate;
     Date endDate; //rango de fechas en el que aplica el precio tarifa indicado.
-    long priceList; //Identificador de la tarifa de precios aplicable.
+    int priceList; //Identificador de la tarifa de precios aplicable.
     long productId; // Identificador código de producto.
     short priority; //Desambiguador de aplicación de precios. Si dos tarifas coinciden en un rago de fechas se aplica la de mayor prioridad (mayor valor numérico).
     BigDecimal price; //precio final de venta.
