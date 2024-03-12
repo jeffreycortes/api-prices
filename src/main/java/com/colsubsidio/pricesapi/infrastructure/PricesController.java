aae41e031d3db7debf14c2bfdf7acf69cea2d16c
Jeffrey Cortés <jeffcorcue@colsubsidio.com>
2024-03-12 03:27:51 -0500

    Se integra PriceManagerService en PricesController

--- src/main/java/com/colsubsidio/pricesapi/infrastructure/PricesController.java
+++ src/main/java/com/colsubsidio/pricesapi/infrastructure/PricesController.java
@@ -1,21 +1,29 @@
 package com.colsubsidio.pricesapi.infrastructure;
 
+import com.colsubsidio.pricesapi.application.PriceManagerService;
 import com.colsubsidio.pricesapi.common.EnvironmentService;
 import com.colsubsidio.pricesapi.common.telemetry.LogsManager;
+import com.colsubsidio.pricesapi.domain.PriceRequestDto;
 import com.colsubsidio.pricesapi.domain.PriceResponseDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import java.math.BigDecimal;
+import java.util.Date;
+
 @RestController
 @RequestMapping("prices/")
 @CrossOrigin(origins = "*")
 public class PricesController  {
-    EnvironmentService environmentService;
+    private final EnvironmentService environmentService;
     private final LogsManager logsManager;
-    PricesController(EnvironmentService environmentService, LogsManager logsManager) {
+    private final PriceManagerService priceManagerService;
+
+    PricesController(EnvironmentService environmentService, LogsManager logsManager, PriceManagerService priceManagerService) {
         this.environmentService = environmentService;
         this.logsManager = logsManager;
+        this.priceManagerService = priceManagerService;
     }
     @GetMapping("/version")
     public String version() {
@@ -26,15 +34,33 @@ public class PricesController  {
     @GetMapping(value = "/{cadenaId}/{productoId}", produces = "application/json")
     @ResponseStatus(HttpStatus.OK)
     public PriceResponseDto getPriceFinal(
-            @PathVariable("cadenaId") String cadenaId,
-            @PathVariable("productoId") String productoId,
-            @RequestParam String dateApply) {
+            @PathVariable("cadenaId") long cadenaId,
+            @PathVariable("productoId") long productoId,
+            @RequestParam Date dateApply) {
         logsManager.info("Fecha aplicacion", dateApply);
         return PriceResponseDto.builder()
                 .cadenaId(cadenaId)
                 .productoId(productoId)
-                .tarifa("35.50")
+                .tarifa(1)
                 .fechasAplicacion(dateApply)
+                .precioFinal(BigDecimal.valueOf(35.500))
+                .moneda("COP")
                 .build();
+    }
+    @GetMapping(value = "/{cadenaId}/{productoId}/query", produces = "application/json")
+    @ResponseStatus(HttpStatus.OK)
+    public PriceResponseDto getPriceEntity(
+            @PathVariable("cadenaId") String cadenaId,
+            @PathVariable("productoId") String productoId,
+            @RequestParam String dateApply) {
+        logsManager.info("Fecha aplicacion", dateApply);
+
+        var filter =  PriceRequestDto.builder()
+                .cadenaId(cadenaId)
+                .productoId(productoId)
+                .fechaAplicacion(dateApply)
+                .build();
+
+        return this.priceManagerService.getFinalPrice(filter);
     }
 }
