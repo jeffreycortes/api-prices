6d452f2894a858f6f76baadd33b320b68da2cef8
Jeffrey Cortés <jeffcorcue@colsubsidio.com>
2024-03-12 05:55:43 -0500

    Se sincronizan tipos de datos en Dtos de req

--- src/main/java/com/colsubsidio/pricesapi/domain/PriceRequestDto.java
+++ src/main/java/com/colsubsidio/pricesapi/domain/PriceRequestDto.java
@@ -3,10 +3,12 @@ package com.colsubsidio.pricesapi.domain;
 import lombok.Builder;
 import lombok.Data;
 
+import java.util.Date;
+
 @Data
 @Builder
 public class PriceRequestDto {
-    String fechaAplicacion;
-    String productoId;
-    String cadenaId;
+    Date fechaAplicacion;
+    long productoId;
+    long cadenaId;
 }
